ec35a8c8fd5105bfda74b0162f62d7e02de87ce0
Adenilson Santos <145278379+Adennilson@users.noreply.github.com>
2025-04-26 20:34:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+    const carouselImages = document.querySelectorAll('.carousel-img img');
+    let currentIndex = 0;
+  
+    function moveCarousel() {
+      carouselImages.forEach((img, index) => {
+        img.style.display = index === currentIndex ? 'block' : 'none';
+      });
+      currentIndex = (currentIndex + 1) % carouselImages.length;
+    }
+  
+    setInterval(moveCarousel, 3000);
+  };
+
+
+const dataInicio = new Date('2024-07-04');
+
+function atualizarCronometro() {
+  const agora = new Date();
+  const diferenca = agora - dataInicio;
+
+  const segundos = Math.floor(diferenca / 1000);
+  const minutos = Math.floor(segundos / 60);
+  const horas = Math.floor(minutos / 60);
+  const dias = Math.floor(horas / 24);
+
+  const segundosRestantes = segundos % 60;
+  const minutosRestantes = minutos % 60;
+  const horasRestantes = horas % 24;
+
+
+  document.getElementById('countdown').innerHTML = `${dias} dias, ${horasRestantes} horas, ${minutosRestantes} minutos e ${segundosRestantes} segundos`;
+}
+
+atualizarCronometro();
+setInterval(atualizarCronometro, 1000);
